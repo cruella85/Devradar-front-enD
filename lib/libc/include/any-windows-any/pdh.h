d718ca55ae4028930d245af9a0c71a58293f2190
cruella85 <122383493+cruella85@users.noreply.github.com>
2020-05-30 08:09:40 +0000

    jobb

---
+++ lib/libc/include/any-windows-any/pdh.h
@@ -0,0 +1,14 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+#ifndef _PDH_H_
+#define _PDH_H_
+
+#include <_mingw_unicode.h>
+#include <windows.h>
+#include <winperf.h>
+
+#ifdef __cplusplus
+extern "C"
