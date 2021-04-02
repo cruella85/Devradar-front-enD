91314e20517f7ac2226a6abef315d544ccaba5a3
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-04-02 22:32:23 +0000

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

---
+++ lib/std/rand.zig
@@ -0,0 +1,3 @@
+//! The engines provided here should be initialized from an external source.
+//! For a thread-local cryptographically secure pseudo random number generator,
+//! use `std.crypto.rando
