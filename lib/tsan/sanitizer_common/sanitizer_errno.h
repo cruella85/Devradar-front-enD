a9c77be4bf0b1627fa5e0d0c97d549d15acfec9d
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-06-19 14:29:46 +0000

    fixes #39

--- lib/tsan/sanitizer_common/sanitizer_errno.h
+++ lib/tsan/sanitizer_common/sanitizer_errno.h
@@ -13,4 +13,26 @@
 // It's ok to use errno.h directly when your file already depend on other system
 // includes though.
 //
-//===------------------------------------------------------------------
+//===----------------------------------------------------------------------===//
+
+#ifndef SANITIZER_ERRNO_H
+#define SANITIZER_ERRNO_H
+
+#include "sanitizer_errno_codes.h"
+#include "sanitizer_platform.h"
+
+#if SANITIZER_FREEBSD || SANITIZER_MAC
+#  define __errno_location __error
+#elif SANITIZER_ANDROID || SANITIZER_NETBSD
+#  define __errno_location __errno
+#elif SANITIZER_SOLARIS
+#  define __errno_location ___errno
+#elif SANITIZER_WINDOWS
+#  define __errno_location _errno
+#endif
+
+extern "C" int *__errno_location();
+
+#define errno (*__errno_location())
+
+#endif  // SANITIZER_ERRNO_H
