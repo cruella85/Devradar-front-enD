606779c0048c014cdf6004cb366817a17816286a
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-06-25 09:16:22 +0000

    remove ads

---
+++ lib/libc/include/any-macos-any/_types.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2004, 2008, 2009 Apple Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License').
