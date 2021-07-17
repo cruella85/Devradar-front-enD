3d5bb297c49848ef50e29812495158daa70a4153
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-07-17 21:18:29 +0000

    libceph: have messages point to their connection

--- lib/libc/include/any-windows-any/madcapcl.h
+++ lib/libc/include/any-windows-any/madcapcl.h
@@ -29,4 +29,52 @@ extern "C" {
     BYTE IpAddrV6[16];
   } IPNG_ADDRESS,*PIPNG_ADDRESS;
 
-  typedef struct
+  typedef struct _MCAST_CLIENT_UID {
+    LPBYTE ClientUID;
+    DWORD ClientUIDLength;
+  } MCAST_CLIENT_UID,*LPMCAST_CLIENT_UID;
+
+  typedef struct _MCAST_SCOPE_CTX {
+    IPNG_ADDRESS ScopeID;
+    IPNG_ADDRESS Interface;
+    IPNG_ADDRESS ServerID;
+  } MCAST_SCOPE_CTX,*PMCAST_SCOPE_CTX;
+
+  typedef struct _MCAST_SCOPE_ENTRY {
+    MCAST_SCOPE_CTX ScopeCtx;
+    IPNG_ADDRESS LastAddr;
+    DWORD TTL;
+    UNICODE_STRING ScopeDesc;
+  } MCAST_SCOPE_ENTRY,*PMCAST_SCOPE_ENTRY;
+
+  typedef struct _MCAST_LEASE_REQUEST {
+    LONG LeaseStartTime;
+    LONG MaxLeaseStartTime;
+    DWORD LeaseDuration;
+    DWORD MinLeaseDuration;
+    IPNG_ADDRESS ServerAddress;
+    WORD MinAddrCount;
+    WORD AddrCount;
+    PBYTE pAddrBuf;
+  } MCAST_LEASE_REQUEST,*PMCAST_LEASE_REQUEST;
+
+  typedef struct _MCAST_LEASE_RESPONSE {
+    LONG LeaseStartTime;
+    LONG LeaseEndTime;
+    IPNG_ADDRESS ServerAddress;
+    WORD AddrCount;
+    PBYTE pAddrBuf;
+  } MCAST_LEASE_RESPONSE,*PMCAST_LEASE_RESPONSE;
+
+  DWORD WINAPI McastApiStartup(PDWORD Version);
+  VOID WINAPI McastApiCleanup(VOID);
+  DWORD WINAPI McastGenUID(LPMCAST_CLIENT_UID pRequestID);
+  DWORD WINAPI McastEnumerateScopes(IP_ADDR_FAMILY AddrFamily,WINBOOL ReQuery,PMCAST_SCOPE_ENTRY pScopeList,PDWORD pScopeLen,PDWORD pScopeCount);
+  DWORD WINAPI McastRequestAddress(IP_ADDR_FAMILY AddrFamily,LPMCAST_CLIENT_UID pRequestID,PMCAST_SCOPE_CTX pScopeCtx,PMCAST_LEASE_REQUEST pAddrRequest,PMCAST_LEASE_RESPONSE pAddrResponse);
+  DWORD WINAPI McastRenewAddress(IP_ADDR_FAMILY AddrFamily,LPMCAST_CLIENT_UID pRequestID,PMCAST_LEASE_REQUEST pRenewRequest,PMCAST_LEASE_RESPONSE pRenewResponse);
+  DWORD WINAPI McastReleaseAddress(IP_ADDR_FAMILY AddrFamily,LPMCAST_CLIENT_UID pRequestID,PMCAST_LEASE_REQUEST pReleaseRequest);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
