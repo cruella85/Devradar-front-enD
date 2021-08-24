c73cb4373f8608b055950aee38a640aa227ffb43
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-08-24 18:02:11 +0000

    tuned proofs

--- lib/libc/mingw/stdio/ftruncate64.c
+++ lib/libc/mingw/stdio/ftruncate64.c
@@ -167,4 +167,205 @@ checkfreespace (const HANDLE f, const ULONGLONG requiredspace)
     _set_errno(EBADF);
     return -1; /* Out of memory */
   }
-  check = GetFinalPathNameByHandleW(f,filepath,check
+  check = GetFinalPathNameByHandleW(f,filepath,check,FILE_NAME_NORMALIZED|VOLUME_NAME_GUID);
+  /* FIXME: last error was set to error 87 (0x57)
+  "The parameter is incorrect." for some reason but works out */
+  if (!check) {
+    _set_errno(EBADF);
+    return -1; /* Error resolving filename */
+  }
+#endif /* _CHECK_SPACE_BY_VISTA_METHOD_ */
+
+#if (_CHECK_SPACE_BY_PSAPI_METHOD_ ==  1)
+  LPWSTR filepath = NULL;
+  filepath = xp_getfilepath(f,currentsize);
+
+  /* Get durectory path */
+  dirpath = getdirpath(filepath);
+  free(filepath);
+  filepath =  NULL;
+  if (!dirpath) {
+    _set_errno(EBADF);
+    return -1; /* Out of memory */
+  }
+#endif /* _CHECK_SPACE_BY_PSAPI_METHOD_ */
+
+#if _CHECK_SPACE_BY_VOLUME_METHOD_
+  if(!GetFileInformationByHandle(f,&fileinfo)) {
+    _set_errno(EINVAL);
+    return -1; /* Resolution failure */
+  }
+
+  volumeid = calloc(51,sizeof(wchar_t));
+  volumepath = calloc(MAX_PATH+2,sizeof(wchar_t));
+  if(!volumeid || !volumepath) {
+  _set_errno(EBADF);
+    return -1; /* Out of memory */
+  }
+
+  dirpath = NULL;
+
+  vol = FindFirstVolumeW(volumeid,50);
+  /* wprintf(L"%d - %ws\n",wcslen(volumeid),volumeid); */
+  do {
+    check = GetVolumeInformationW(volumeid,volumepath,MAX_PATH+1,&volumeserial,NULL,NULL,NULL,0);
+    /* wprintf(L"GetVolumeInformationW %d id %ws path %ws error %d\n",check,volumeid,volumepath,GetLastError()); */
+    if(volumeserial == fileinfo.dwVolumeSerialNumber) {
+      dirpath = volumeid; 
+      break;
+    }
+  } while (FindNextVolumeW(vol,volumeid,50));
+  FindVolumeClose(vol);
+
+  if(!dirpath) free(volumeid); /* we found the volume */
+  free(volumepath);
+#endif /* _CHECK_SPACE_BY_VOLUME_METHOD_ */
+
+  /* Get available free space */
+  check = GetDiskFreeSpaceExW(dirpath,&freespace,NULL,NULL);
+  //wprintf(L"freespace %I64u\n",freespace);
+  free(dirpath);
+  if(!check) {
+    _set_errno(EFBIG);
+    return -1; /* Error getting free space */
+  }
+ 
+  /* Check space requirements */
+  if ((requiredspace - currentsize.QuadPart) > freespace.QuadPart)
+  {
+    _set_errno(EFBIG); /* File too big for disk */
+    return -1;
+  } /* We have enough space to truncate/expand */
+  return 0;
+}
+
+int ftruncate64(int __fd, _off64_t __length) {
+  HANDLE f;
+  LARGE_INTEGER quad;
+  DWORD check;
+  int ret = 0;
+  __int64 pos;
+
+  /* Sanity check */
+  if (__length < 0) {
+    goto errorout;
+  }
+
+  /* Get Win32 Handle */
+  if(__fd == -1) {
+    goto errorout;
+  }
+
+  f = (HANDLE)_get_osfhandle(__fd);
+  if (f == INVALID_HANDLE_VALUE || (GetFileType(f) != FILE_TYPE_DISK)) {
+    _set_errno(EBADF);
+    return -1;
+  }
+
+
+  /* Save position */
+  if((pos = _telli64(__fd)) == -1LL){
+    goto errorout;
+  }
+
+  /* Check available space */
+  check = checkfreespace(f,__length);
+  if (check != 0) {
+    return -1; /* Error, errno already set */
+  }
+
+  quad.QuadPart = __length;
+  check = SetFilePointer(f, (LONG)quad.LowPart, &(quad.HighPart), FILE_BEGIN);
+  if (check == INVALID_SET_FILE_POINTER && quad.LowPart != INVALID_SET_FILE_POINTER) {
+    switch (GetLastError()) {
+      case ERROR_NEGATIVE_SEEK:
+        _set_errno(EFBIG); /* file too big? */
+        return -1;
+      case INVALID_SET_FILE_POINTER:
+        _set_errno(EINVAL); /* shouldn't happen */
+        return -1;
+      default:
+        _set_errno(EINVAL); /* shouldn't happen */
+        return -1;
+    }
+  }
+
+  check = SetEndOfFile(f);
+  if (!check) {
+    goto errorout;
+  }
+
+  if(_lseeki64(__fd,pos,SEEK_SET) == -1LL){
+    goto errorout;
+  }
+
+  return ret;
+
+  errorout:
+  _set_errno(EINVAL);
+  return -1;
+}
+
+#if (TEST_FTRUNCATE64 == 1)
+int main(){
+  LARGE_INTEGER sz;
+  ULARGE_INTEGER freespace;
+  int f;
+  LPWSTR path, dir;
+  sz.QuadPart = 0LL;
+  f = _open("XXX.tmp", _O_BINARY|_O_CREAT|_O_RDWR, _S_IREAD | _S_IWRITE);
+  wprintf(L"%d\n",ftruncate64(f,12));
+  wprintf(L"%d\n",ftruncate64(f,20));
+  wprintf(L"%d\n",ftruncate64(f,15));
+/*  path = xp_getfilepath((HANDLE)_get_osfhandle(f),sz);
+  dir = getdirpath(path);
+  GetDiskFreeSpaceExW(dir,&freespace,NULL,NULL);
+  wprintf(L"fs - %ws\n",path);
+  wprintf(L"dirfs - %ws\n",dir);
+  wprintf(L"free - %I64u\n",freespace.QuadPart);
+  free(dir);
+  free(path);*/
+  _close(f);
+  return 0;
+}
+#endif /* TEST_FTRUNCATE64 */
+
+#if (TEST_FTRUNCATE64 == 2)
+int main() {
+FILE *f;
+int fd;
+char buf[100];
+int cnt;
+unlink("test.out");
+f = fopen("test.out","w+");
+fd = fileno(f);
+write(fd,"abc",3);
+fflush(f);
+printf ("err: %d\n", ftruncate64(fd,10));
+cnt = read(fd,buf,100);
+printf("cnt = %d\n",cnt);
+return 0;
+}
+#endif /* TEST_FTRUNCATE64 */
+
+#if (TEST_FTRUNCATE64 == 3)
+int main() {
+FILE *f;
+int fd;
+char buf[100];
+int cnt;
+unlink("test.out");
+f = fopen("test.out","w+");
+fd = fileno(f);
+write(fd,"abc",3);
+fflush(f);
+ftruncate64(fd,0);
+write(fd,"def",3);
+fclose(f);
+f = fopen("test.out","r");
+cnt = fread(buf,1,100,f);
+printf("cnt = %d\n",cnt);
+return 0;
+}
+#endif /* TEST_FTRUNCATE64 */
+
