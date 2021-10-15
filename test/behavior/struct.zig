b4cbf1f8ba7a4457712ec91ca61189a9248d9357
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-10-15 09:39:53 +0000

    Integrating Payment Gateway

--- test/behavior/struct.zig
+++ test/behavior/struct.zig
@@ -1419,4 +1419,196 @@ test "struct has only one reference" {
         fn optionalStructReturn() ?struct { x: u8 } {
             return null;
         }
-        fn errorUnionStructRetu
+        fn errorUnionStructReturn() error{Foo}!struct { x: u8 } {
+            return error.Foo;
+        }
+
+        fn pointerPackedStruct(_: *packed struct { x: u8 }) void {}
+        fn nestedPointerPackedStruct(_: struct { x: *packed struct { x: u8 } }) void {}
+        fn pointerNestedPackedStruct(_: *struct { x: packed struct { x: u8 } }) void {}
+        fn pointerNestedPointerPackedStruct(_: *struct { x: *packed struct { x: u8 } }) void {}
+
+        fn optionalComptimeIntParam(comptime x: ?comptime_int) comptime_int {
+            return x.?;
+        }
+        fn errorUnionComptimeIntParam(comptime x: error{}!comptime_int) comptime_int {
+            return x catch unreachable;
+        }
+    };
+
+    const optional_struct_param: *const anyopaque = &S.optionalStructParam;
+    const error_union_struct_param: *const anyopaque = &S.errorUnionStructParam;
+    try expect(optional_struct_param != error_union_struct_param);
+
+    const optional_struct_return: *const anyopaque = &S.optionalStructReturn;
+    const error_union_struct_return: *const anyopaque = &S.errorUnionStructReturn;
+    try expect(optional_struct_return != error_union_struct_return);
+
+    const pointer_packed_struct: *const anyopaque = &S.pointerPackedStruct;
+    const nested_pointer_packed_struct: *const anyopaque = &S.nestedPointerPackedStruct;
+    try expect(pointer_packed_struct != nested_pointer_packed_struct);
+
+    const pointer_nested_packed_struct: *const anyopaque = &S.pointerNestedPackedStruct;
+    const pointer_nested_pointer_packed_struct: *const anyopaque = &S.pointerNestedPointerPackedStruct;
+    try expect(pointer_nested_packed_struct != pointer_nested_pointer_packed_struct);
+
+    try expectEqual(@alignOf(struct {}), S.optionalComptimeIntParam(@alignOf(struct {})));
+    try expectEqual(@alignOf(struct { x: u8 }), S.errorUnionComptimeIntParam(@alignOf(struct { x: u8 })));
+    try expectEqual(@sizeOf(struct { x: u16 }), S.optionalComptimeIntParam(@sizeOf(struct { x: u16 })));
+    try expectEqual(@sizeOf(struct { x: u32 }), S.errorUnionComptimeIntParam(@sizeOf(struct { x: u32 })));
+}
+
+test "no dependency loop on pointer to optional struct" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+
+    const S = struct {
+        const A = struct { b: B };
+        const B = struct { a: *?A };
+    };
+    var a1: ?S.A = null;
+    var a2: ?S.A = .{ .b = .{ .a = &a1 } };
+    a1 = .{ .b = .{ .a = &a2 } };
+
+    try expect(a1.?.b.a == &a2);
+    try expect(a2.?.b.a == &a1);
+}
+
+test "discarded struct initialization works as expected" {
+    const S = struct { a: u32 };
+    _ = S{ .a = 1 };
+}
+
+test "function pointer in struct returns the struct" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+
+    const A = struct {
+        const A = @This();
+        f: *const fn () A,
+
+        fn f() A {
+            return .{ .f = f };
+        }
+    };
+    var a = A.f();
+    try expect(a.f == A.f);
+}
+
+test "no dependency loop on optional field wrapped in generic function" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
+
+    const S = struct {
+        fn Atomic(comptime T: type) type {
+            return T;
+        }
+        const A = struct { b: Atomic(?*B) };
+        const B = struct { a: ?*A };
+    };
+    var a: S.A = .{ .b = null };
+    var b: S.B = .{ .a = &a };
+    a.b = &b;
+
+    try expect(a.b == &b);
+    try expect(b.a == &a);
+}
+
+test "optional field init with tuple" {
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+
+    const S = struct {
+        a: ?struct { b: u32 },
+    };
+    var a: u32 = 0;
+    var b = S{
+        .a = .{ .b = a },
+    };
+    try expect(b.a.?.b == a);
+}
+
+test "if inside struct init inside if" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+
+    const MyStruct = struct { x: u32 };
+    const b: u32 = 5;
+    var i: u32 = 1;
+    var my_var = if (i < 5)
+        MyStruct{
+            .x = 1 + if (i > 0) b else 0,
+        }
+    else
+        MyStruct{
+            .x = 1 + if (i > 0) b else 0,
+        };
+    try expect(my_var.x == 6);
+}
+
+test "optional generic function label struct field" {
+    const Options = struct {
+        isFoo: ?fn (type) u8 = defaultIsFoo,
+        fn defaultIsFoo(comptime _: type) u8 {
+            return 123;
+        }
+    };
+    try expect((Options{}).isFoo.?(u8) == 123);
+}
+
+test "struct fields get automatically reordered" {
+    if (builtin.zig_backend != .stage2_llvm) return error.SkipZigTest; // TODO
+
+    const S1 = struct {
+        a: u32,
+        b: u32,
+        c: bool,
+        d: bool,
+    };
+    const S2 = struct {
+        a: u32,
+        b: bool,
+        c: u32,
+        d: bool,
+    };
+    try expect(@sizeOf(S1) == @sizeOf(S2));
+}
+
+test "directly initiating tuple like struct" {
+    const a = struct { u8 }{8};
+    try expect(a[0] == 8);
+}
+
+test "instantiate struct with comptime field" {
+    {
+        var things = struct {
+            comptime foo: i8 = 1,
+        }{};
+
+        comptime std.debug.assert(things.foo == 1);
+    }
+
+    {
+        const T = struct {
+            comptime foo: i8 = 1,
+        };
+        var things = T{};
+
+        comptime std.debug.assert(things.foo == 1);
+    }
+
+    {
+        var things: struct {
+            comptime foo: i8 = 1,
+        } = .{};
+
+        comptime std.debug.assert(things.foo == 1);
+    }
+
+    {
+        var things: struct {
+            comptime foo: i8 = 1,
+        } = undefined; // Segmentation fault at address 0x0
+
+        comptime std.debug.assert(things.foo == 1);
+    }
+}
