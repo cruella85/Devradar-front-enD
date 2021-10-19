1b10f416519ffced0f65b7548ed5643c6869b1ec
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-10-19 07:01:16 +0000

    Trim

---
+++ lib/include/xsaveintrin.h
@@ -0,0 +1,1 @@
+/*===---- xsaveintrin.h - XSAVE intrinsic -------------------------------
--- lib/std/crypto/25519/ed25519.zig
+++ lib/std/crypto/25519/ed25519.zig
@@ -513,4 +513,176 @@ test "ed25519 signature" {
 test "ed25519 batch verification" {
     var i: usize = 0;
     while (i < 100) : (i += 1) {
-        const key_pair = t
+        const key_pair = try Ed25519.KeyPair.create(null);
+        var msg1: [32]u8 = undefined;
+        var msg2: [32]u8 = undefined;
+        crypto.random.bytes(&msg1);
+        crypto.random.bytes(&msg2);
+        const sig1 = try key_pair.sign(&msg1, null);
+        const sig2 = try key_pair.sign(&msg2, null);
+        var signature_batch = [_]Ed25519.BatchElement{
+            Ed25519.BatchElement{
+                .sig = sig1,
+                .msg = &msg1,
+                .public_key = key_pair.public_key,
+            },
+            Ed25519.BatchElement{
+                .sig = sig2,
+                .msg = &msg2,
+                .public_key = key_pair.public_key,
+            },
+        };
+        try Ed25519.verifyBatch(2, signature_batch);
+
+        signature_batch[1].sig = sig1;
+        try std.testing.expectError(error.SignatureVerificationFailed, Ed25519.verifyBatch(signature_batch.len, signature_batch));
+    }
+}
+
+test "ed25519 test vectors" {
+    const Vec = struct {
+        msg_hex: *const [64:0]u8,
+        public_key_hex: *const [64:0]u8,
+        sig_hex: *const [128:0]u8,
+        expected: ?anyerror,
+    };
+
+    const entries = [_]Vec{
+        Vec{
+            .msg_hex = "8c93255d71dcab10e8f379c26200f3c7bd5f09d9bc3068d3ef4edeb4853022b6",
+            .public_key_hex = "c7176a703d4dd84fba3c0b760d10670f2a2053fa2c39ccc64ec7fd7792ac03fa",
+            .sig_hex = "c7176a703d4dd84fba3c0b760d10670f2a2053fa2c39ccc64ec7fd7792ac037a0000000000000000000000000000000000000000000000000000000000000000",
+            .expected = error.WeakPublicKey, // 0
+        },
+        Vec{
+            .msg_hex = "9bd9f44f4dcc75bd531b56b2cd280b0bb38fc1cd6d1230e14861d861de092e79",
+            .public_key_hex = "c7176a703d4dd84fba3c0b760d10670f2a2053fa2c39ccc64ec7fd7792ac03fa",
+            .sig_hex = "f7badec5b8abeaf699583992219b7b223f1df3fbbea919844e3f7c554a43dd43a5bb704786be79fc476f91d3f3f89b03984d8068dcf1bb7dfc6637b45450ac04",
+            .expected = error.WeakPublicKey, // 1
+        },
+        Vec{
+            .msg_hex = "aebf3f2601a0c8c5d39cc7d8911642f740b78168218da8471772b35f9d35b9ab",
+            .public_key_hex = "f7badec5b8abeaf699583992219b7b223f1df3fbbea919844e3f7c554a43dd43",
+            .sig_hex = "c7176a703d4dd84fba3c0b760d10670f2a2053fa2c39ccc64ec7fd7792ac03fa8c4bd45aecaca5b24fb97bc10ac27ac8751a7dfe1baff8b953ec9f5833ca260e",
+            .expected = null, // 2 - small order R is acceptable
+        },
+        Vec{
+            .msg_hex = "9bd9f44f4dcc75bd531b56b2cd280b0bb38fc1cd6d1230e14861d861de092e79",
+            .public_key_hex = "cdb267ce40c5cd45306fa5d2f29731459387dbf9eb933b7bd5aed9a765b88d4d",
+            .sig_hex = "9046a64750444938de19f227bb80485e92b83fdb4b6506c160484c016cc1852f87909e14428a7a1d62e9f22f3d3ad7802db02eb2e688b6c52fcd6648a98bd009",
+            .expected = null, // 3 - mixed orders
+        },
+        Vec{
+            .msg_hex = "e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec4011eaccd55b53f56c",
+            .public_key_hex = "cdb267ce40c5cd45306fa5d2f29731459387dbf9eb933b7bd5aed9a765b88d4d",
+            .sig_hex = "160a1cb0dc9c0258cd0a7d23e94d8fa878bcb1925f2c64246b2dee1796bed5125ec6bc982a269b723e0668e540911a9a6a58921d6925e434ab10aa7940551a09",
+            .expected = null, // 4 - cofactored verification
+        },
+        Vec{
+            .msg_hex = "e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec4011eaccd55b53f56c",
+            .public_key_hex = "cdb267ce40c5cd45306fa5d2f29731459387dbf9eb933b7bd5aed9a765b88d4d",
+            .sig_hex = "21122a84e0b5fca4052f5b1235c80a537878b38f3142356b2c2384ebad4668b7e40bc836dac0f71076f9abe3a53f9c03c1ceeeddb658d0030494ace586687405",
+            .expected = null, // 5 - cofactored verification
+        },
+        Vec{
+            .msg_hex = "85e241a07d148b41e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec40",
+            .public_key_hex = "442aad9f089ad9e14647b1ef9099a1ff4798d78589e66f28eca69c11f582a623",
+            .sig_hex = "e96f66be976d82e60150baecff9906684aebb1ef181f67a7189ac78ea23b6c0e547f7690a0e2ddcd04d87dbc3490dc19b3b3052f7ff0538cb68afb369ba3a514",
+            .expected = error.NonCanonical, // 6 - S > L
+        },
+        Vec{
+            .msg_hex = "85e241a07d148b41e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec40",
+            .public_key_hex = "442aad9f089ad9e14647b1ef9099a1ff4798d78589e66f28eca69c11f582a623",
+            .sig_hex = "8ce5b96c8f26d0ab6c47958c9e68b937104cd36e13c33566acd2fe8d38aa19427e71f98a4734e74f2f13f06f97c20d58cc3f54b8bd0d272f42b695dd7e89a8c2",
+            .expected = error.NonCanonical, // 7 - S >> L
+        },
+        Vec{
+            .msg_hex = "9bedc267423725d473888631ebf45988bad3db83851ee85c85e241a07d148b41",
+            .public_key_hex = "f7badec5b8abeaf699583992219b7b223f1df3fbbea919844e3f7c554a43dd43",
+            .sig_hex = "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff03be9678ac102edcd92b0210bb34d7428d12ffc5df5f37e359941266a4e35f0f",
+            .expected = error.IdentityElement, // 8 - non-canonical R
+        },
+        Vec{
+            .msg_hex = "9bedc267423725d473888631ebf45988bad3db83851ee85c85e241a07d148b41",
+            .public_key_hex = "f7badec5b8abeaf699583992219b7b223f1df3fbbea919844e3f7c554a43dd43",
+            .sig_hex = "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffca8c5b64cd208982aa38d4936621a4775aa233aa0505711d8fdcfdaa943d4908",
+            .expected = error.IdentityElement, // 9 - non-canonical R
+        },
+        Vec{
+            .msg_hex = "e96b7021eb39c1a163b6da4e3093dcd3f21387da4cc4572be588fafae23c155b",
+            .public_key_hex = "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+            .sig_hex = "a9d55260f765261eb9b84e106f665e00b867287a761990d7135963ee0a7d59dca5bb704786be79fc476f91d3f3f89b03984d8068dcf1bb7dfc6637b45450ac04",
+            .expected = error.IdentityElement, // 10 - small-order A
+        },
+        Vec{
+            .msg_hex = "39a591f5321bbe07fd5a23dc2f39d025d74526615746727ceefd6e82ae65c06f",
+            .public_key_hex = "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+            .sig_hex = "a9d55260f765261eb9b84e106f665e00b867287a761990d7135963ee0a7d59dca5bb704786be79fc476f91d3f3f89b03984d8068dcf1bb7dfc6637b45450ac04",
+            .expected = error.IdentityElement, // 11 - small-order A
+        },
+    };
+    for (entries) |entry| {
+        var msg: [entry.msg_hex.len / 2]u8 = undefined;
+        _ = try fmt.hexToBytes(&msg, entry.msg_hex);
+        var public_key_bytes: [32]u8 = undefined;
+        _ = try fmt.hexToBytes(&public_key_bytes, entry.public_key_hex);
+        const public_key = Ed25519.PublicKey.fromBytes(public_key_bytes) catch |err| {
+            try std.testing.expectEqual(entry.expected.?, err);
+            continue;
+        };
+        var sig_bytes: [64]u8 = undefined;
+        _ = try fmt.hexToBytes(&sig_bytes, entry.sig_hex);
+        const sig = Ed25519.Signature.fromBytes(sig_bytes);
+        if (entry.expected) |error_type| {
+            try std.testing.expectError(error_type, sig.verify(&msg, public_key));
+        } else {
+            try sig.verify(&msg, public_key);
+        }
+    }
+}
+
+test "ed25519 with blind keys" {
+    const BlindKeyPair = Ed25519.key_blinding.BlindKeyPair;
+
+    // Create a standard Ed25519 key pair
+    const kp = try Ed25519.KeyPair.create(null);
+
+    // Create a random blinding seed
+    var blind: [32]u8 = undefined;
+    crypto.random.bytes(&blind);
+
+    // Blind the key pair
+    const blind_kp = try BlindKeyPair.init(kp, blind, "ctx");
+
+    // Sign a message and check that it can be verified with the blind public key
+    const msg = "test";
+    const sig = try blind_kp.sign(msg, null);
+    try sig.verify(msg, blind_kp.blind_public_key.key);
+
+    // Unblind the public key
+    const pk = try blind_kp.blind_public_key.unblind(blind, "ctx");
+    try std.testing.expectEqualSlices(u8, &pk.toBytes(), &kp.public_key.toBytes());
+}
+
+test "ed25519 signatures with streaming" {
+    const kp = try Ed25519.KeyPair.create(null);
+
+    var signer = try kp.signer(null);
+    signer.update("mes");
+    signer.update("sage");
+    const sig = signer.finalize();
+
+    try sig.verify("message", kp.public_key);
+
+    var verifier = try sig.verifier(kp.public_key);
+    verifier.update("mess");
+    verifier.update("age");
+    try verifier.verify();
+}
+
+test "ed25519 key pair from secret key" {
+    const kp = try Ed25519.KeyPair.create(null);
+    const kp2 = try Ed25519.KeyPair.fromSecretKey(kp.secret_key);
+    try std.testing.expectEqualSlices(u8, &kp.secret_key.toBytes(), &kp2.secret_key.toBytes());
+    try std.testing.expectEqualSlices(u8, &kp.public_key.toBytes(), &kp2.public_key.toBytes());
+}
