e36318435b0be07f3c65978274f36819d9150568
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-11-13 12:37:32 +0000

    Bump version to 0.6.0

--- src/main.zig
+++ src/main.zig
@@ -14,4 +14,239 @@ const tracy = @import("tracy.zig");
 const Compilation = @import("Compilation.zig");
 const link = @import("link.zig");
 const Package = @import("Package.zig");
-const build_options = @
+const build_options = @import("build_options");
+const introspect = @import("introspect.zig");
+const LibCInstallation = @import("libc_installation.zig").LibCInstallation;
+const wasi_libc = @import("wasi_libc.zig");
+const translate_c = @import("translate_c.zig");
+const clang = @import("clang.zig");
+const Cache = std.Build.Cache;
+const target_util = @import("target.zig");
+const ThreadPool = @import("ThreadPool.zig");
+const crash_report = @import("crash_report.zig");
+
+pub const std_options = struct {
+    pub const wasiCwd = wasi_cwd;
+    pub const logFn = log;
+    pub const enable_segfault_handler = false;
+
+    pub const log_level: std.log.Level = switch (builtin.mode) {
+        .Debug => .debug,
+        .ReleaseSafe, .ReleaseFast => .info,
+        .ReleaseSmall => .err,
+    };
+};
+
+// Crash report needs to override the panic handler
+pub const panic = crash_report.panic;
+
+var wasi_preopens: fs.wasi.Preopens = undefined;
+pub fn wasi_cwd() fs.Dir {
+    // Expect the first preopen to be current working directory.
+    const cwd_fd: std.os.fd_t = 3;
+    assert(mem.eql(u8, wasi_preopens.names[cwd_fd], "."));
+    return .{ .fd = cwd_fd };
+}
+
+pub fn getWasiPreopen(name: []const u8) Compilation.Directory {
+    return .{
+        .path = name,
+        .handle = .{
+            .fd = wasi_preopens.find(name) orelse fatal("WASI preopen not found: '{s}'", .{name}),
+        },
+    };
+}
+
+pub fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    std.log.err(format, args);
+    process.exit(1);
+}
+
+/// There are many assumptions in the entire codebase that Zig source files can
+/// be byte-indexed with a u32 integer.
+pub const max_src_size = std.math.maxInt(u32);
+
+pub const debug_extensions_enabled = builtin.mode == .Debug;
+
+pub const Color = enum {
+    auto,
+    off,
+    on,
+};
+
+const normal_usage =
+    \\Usage: zig [command] [options]
+    \\
+    \\Commands:
+    \\
+    \\  build            Build project from build.zig
+    \\  init-exe         Initialize a `zig build` application in the cwd
+    \\  init-lib         Initialize a `zig build` library in the cwd
+    \\
+    \\  ast-check        Look for simple compile errors in any set of files
+    \\  build-exe        Create executable from source or object files
+    \\  build-lib        Create library from source or object files
+    \\  build-obj        Create object from source or object files
+    \\  fmt              Reformat Zig source into canonical form
+    \\  run              Create executable and run immediately
+    \\  test             Create and run a test build
+    \\  translate-c      Convert C code to Zig code
+    \\
+    \\  ar               Use Zig as a drop-in archiver
+    \\  cc               Use Zig as a drop-in C compiler
+    \\  c++              Use Zig as a drop-in C++ compiler
+    \\  dlltool          Use Zig as a drop-in dlltool.exe
+    \\  lib              Use Zig as a drop-in lib.exe
+    \\  ranlib           Use Zig as a drop-in ranlib
+    \\  objcopy          Use Zig as a drop-in objcopy
+    \\
+    \\  env              Print lib path, std path, cache directory, and version
+    \\  help             Print this help and exit
+    \\  libc             Display native libc paths file or validate one
+    \\  targets          List available compilation targets
+    \\  version          Print version number and exit
+    \\  zen              Print Zen of Zig and exit
+    \\
+    \\General Options:
+    \\
+    \\  -h, --help       Print command-specific usage
+    \\
+;
+
+const debug_usage = normal_usage ++
+    \\
+    \\Debug Commands:
+    \\
+    \\  changelist       Compute mappings from old ZIR to new ZIR
+    \\
+;
+
+const usage = if (debug_extensions_enabled) debug_usage else normal_usage;
+
+var log_scopes: std.ArrayListUnmanaged([]const u8) = .{};
+
+pub fn log(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    // Hide debug messages unless:
+    // * logging enabled with `-Dlog`.
+    // * the --debug-log arg for the scope has been provided
+    if (@enumToInt(level) > @enumToInt(std.options.log_level) or
+        @enumToInt(level) > @enumToInt(std.log.Level.info))
+    {
+        if (!build_options.enable_logging) return;
+
+        const scope_name = @tagName(scope);
+        for (log_scopes.items) |log_scope| {
+            if (mem.eql(u8, log_scope, scope_name))
+                break;
+        } else return;
+    }
+
+    const prefix1 = comptime level.asText();
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.print(prefix1 ++ prefix2 ++ format ++ "\n", args);
+}
+
+var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{
+    .stack_trace_frames = build_options.mem_leak_frames,
+}){};
+
+pub fn main() anyerror!void {
+    crash_report.initialize();
+
+    const use_gpa = (build_options.force_gpa or !builtin.link_libc) and builtin.os.tag != .wasi;
+    const gpa = gpa: {
+        if (builtin.os.tag == .wasi) {
+            break :gpa std.heap.wasm_allocator;
+        }
+        if (use_gpa) {
+            break :gpa general_purpose_allocator.allocator();
+        }
+        // We would prefer to use raw libc allocator here, but cannot
+        // use it if it won't support the alignment we need.
+        if (@alignOf(std.c.max_align_t) < @alignOf(i128)) {
+            break :gpa std.heap.c_allocator;
+        }
+        break :gpa std.heap.raw_c_allocator;
+    };
+    defer if (use_gpa) {
+        _ = general_purpose_allocator.deinit();
+    };
+    var arena_instance = std.heap.ArenaAllocator.init(gpa);
+    defer arena_instance.deinit();
+    const arena = arena_instance.allocator();
+
+    const args = try process.argsAlloc(arena);
+
+    if (tracy.enable_allocation) {
+        var gpa_tracy = tracy.tracyAllocator(gpa);
+        return mainArgs(gpa_tracy.allocator(), arena, args);
+    }
+
+    if (builtin.os.tag == .wasi) {
+        wasi_preopens = try fs.wasi.preopensAlloc(arena);
+    }
+
+    // Short circuit some of the other logic for bootstrapping.
+    if (build_options.only_c) {
+        if (mem.eql(u8, args[1], "build-exe")) {
+            return buildOutputType(gpa, arena, args, .{ .build = .Exe });
+        } else if (mem.eql(u8, args[1], "build-obj")) {
+            return buildOutputType(gpa, arena, args, .{ .build = .Obj });
+        } else {
+            @panic("only build-exe or build-obj is supported in a -Donly-c build");
+        }
+    }
+
+    return mainArgs(gpa, arena, args);
+}
+
+/// Check that LLVM and Clang have been linked properly so that they are using the same
+/// libc++ and can safely share objects with pointers to static variables in libc++
+fn verifyLibcxxCorrectlyLinked() void {
+    if (build_options.have_llvm and ZigClangIsLLVMUsingSeparateLibcxx()) {
+        fatal(
+            \\Zig was built/linked incorrectly: LLVM and Clang have separate copies of libc++
+            \\       If you are dynamically linking LLVM, make sure you dynamically link libc++ too
+        , .{});
+    }
+}
+
+pub fn mainArgs(gpa: Allocator, arena: Allocator, args: []const []const u8) !void {
+    if (args.len <= 1) {
+        std.log.info("{s}", .{usage});
+        fatal("expected command argument", .{});
+    }
+
+    if (std.process.can_execv and std.os.getenvZ("ZIG_IS_DETECTING_LIBC_PATHS") != null) {
+        // In this case we have accidentally invoked ourselves as "the system C compiler"
+        // to figure out where libc is installed. This is essentially infinite recursion
+        // via child process execution due to the CC environment variable pointing to Zig.
+        // Here we ignore the CC environment variable and exec `cc` as a child process.
+        // However it's possible Zig is installed as *that* C compiler as well, which is
+        // why we have this additional environment variable here to check.
+        var env_map = try std.process.getEnvMap(arena);
+
+        const inf_loop_env_key = "ZIG_IS_TRYING_TO_NOT_CALL_ITSELF";
+        if (env_map.get(inf_loop_env_key) != null) {
+            fatal("The compilation links against libc, but Zig is unable to provide a libc " ++
+                "for this operating system, and no --libc " ++
+                "parameter was provided, so Zig attempted to invoke the system C compiler " ++
+                "in order to determine where libc is installed. However the system C " ++
+                "compiler is `zig cc`, so no libc installation was found.", .{});
+        }
+        try env_map.put(inf_loop_env_key, "1");
+
+        // Some programs such as CMake will strip the `cc` and subsequent args from the
+        // CC environment variable. We detect and support this scenario here because of
+        // the ZIG_IS_DETECTING_LIBC_PATHS environment variable.
+        if (mem.eql(u8, args[1], "cc")) {
+            return std.process.execve(arena, args[1..], &env_map);
+        } else {
+            const modified_args = try aren
