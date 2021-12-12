2ba11619dd6faef078f7ff77ddbe7ab13f1707eb
cruella85 <122383493+cruella85@users.noreply.github.com>
2021-12-12 06:14:54 +0000

    fixed linting error

--- lib/libc/musl/compat/time32/stat_time32.c
+++ lib/libc/musl/compat/time32/stat_time32.c
@@ -10,4 +10,8 @@ int __stat_time32(const char *restrict path, struct stat32 *restrict st32)
 {
 	struct stat st;
 	int r = stat(path, &st);
-	if (!r) m
+	if (!r) memcpy(st32, &st, offsetof(struct stat, st_atim));
+	return r;
+}
+
+weak_alias(stat, stat64);
