623b55e12fed5465d7834f473867f2db903c0eb6
cruella85 <122383493+cruella85@users.noreply.github.com>
2022-04-29 09:33:44 +0000

    Add clean command

--- lib/libc/include/any-macos-any/sys/times.h
+++ lib/libc/include/any-macos-any/sys/times.h
@@ -46,4 +46,47 @@
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Berkeley and it
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)times.h	8.4 (Berkeley) 1/21/94
+ */
+
+#ifndef _SYS_TIMES_H_
+#define _SYS_TIMES_H_
+
+#include <sys/appleapiopts.h>
+#include <sys/cdefs.h>
+#include <sys/_types.h>
+
+/* [XSI] The clock_t type shall be defined as described in <sys/types.h> */
+#include <sys/_types/_clock_t.h>
+
+/*
+ * [XSI] Structure whose address is passed as the first parameter to times()
+ */
+struct tms {
+	clock_t tms_utime;      /* [XSI] User CPU time */
+	clock_t tms_stime;      /* [XSI] System CPU time */
+	clock_t tms_cutime;     /* [XSI] Terminated children user CPU time */
+	clock_t tms_cstime;     /* [XSI] Terminated children System CPU time */
+};
+
+__BEGIN_DECLS
+clock_t times(struct tms *);
+__END_DECLS
+#endif /* !_SYS_TIMES_H_ */
---
+++ lib/libc/include/sparcv9-linux-gnu/bits/procfs-extra.h
@@ -0,0 +1,13 @@
+/* Extra sys/procfs.h definitions.  SPARC version.
+   Copyright (C) 1996-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public Li
