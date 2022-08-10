6cefe57763b829ccf7052c18fb27355c8715b3cc
cruella85 <122383493+cruella85@users.noreply.github.com>
2022-08-10 09:54:40 +0000

    Update configure.py

--- lib/libc/include/any-windows-any/gpmgmt.h
+++ lib/libc/include/any-windows-any/gpmgmt.h
@@ -411,4 +411,172 @@ typedef struct GPMConstants GPMConstants;
 #ifdef __cplusplus
 typedef class GPMResult GPMResult;
 #else
-typedef struct GPMR
+typedef struct GPMResult GPMResult;
+#endif
+#endif
+
+#ifndef __GPMMapEntryCollection_FWD_DEFINED__
+#define __GPMMapEntryCollection_FWD_DEFINED__
+#ifdef __cplusplus
+typedef class GPMMapEntryCollection GPMMapEntryCollection;
+#else
+typedef struct GPMMapEntryCollection GPMMapEntryCollection;
+#endif
+#endif
+
+#ifndef __GPMMapEntry_FWD_DEFINED__
+#define __GPMMapEntry_FWD_DEFINED__
+#ifdef __cplusplus
+typedef class GPMMapEntry GPMMapEntry;
+#else
+typedef struct GPMMapEntry GPMMapEntry;
+#endif
+#endif
+
+#ifndef __GPMMigrationTable_FWD_DEFINED__
+#define __GPMMigrationTable_FWD_DEFINED__
+#ifdef __cplusplus
+typedef class GPMMigrationTable GPMMigrationTable;
+#else
+typedef struct GPMMigrationTable GPMMigrationTable;
+#endif
+#endif
+
+#ifndef __GPOReportProvider_FWD_DEFINED__
+#define __GPOReportProvider_FWD_DEFINED__
+#ifdef __cplusplus
+typedef class GPOReportProvider GPOReportProvider;
+#else
+typedef struct GPOReportProvider GPOReportProvider;
+#endif
+#endif
+
+#include "oaidl.h"
+#include "ocidl.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifndef __MIDL_user_allocate_free_DEFINED__
+#define __MIDL_user_allocate_free_DEFINED__
+  void *__RPC_API MIDL_user_allocate(size_t);
+  void __RPC_API MIDL_user_free(void *);
+#endif
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0001 {
+    rsopUnknown = 0,rsopPlanning,rsopLogging
+  } GPMRSOPMode;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0002 {
+    permGPOApply = 0x10000,permGPORead = 0x10100,permGPOEdit = 0x10101,permGPOEditSecurityAndDelete = 0x10102,permGPOCustom = 0x10103,
+    permWMIFilterEdit = 0x20000,permWMIFilterFullControl = 0x20001,permWMIFilterCustom = 0x20002,permSOMLink = 0x1c0000,permSOMLogging = 0x180100,
+    permSOMPlanning = 0x180200,permSOMWMICreate = 0x100300,permSOMWMIFullControl = 0x100301,permSOMGPOCreate = 0x100400,
+    permStarterGPORead = 0x30500,permStarterGPOEdit = 0x30501,permStarterGPOFullControl = 0x30502,permStarterGPOCustom = 0x30503,
+    permSOMStarterGPOCreate = 0x100500
+  } GPMPermissionType;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0003 {
+    gpoPermissions = 0,gpoEffectivePermissions,gpoDisplayName,gpoWMIFilter,
+    gpoID,gpoComputerExtensions,gpoUserExtensions,somLinks,gpoDomain,
+    backupMostRecent
+  } GPMSearchProperty;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0004 {
+    opEquals = 0,opContains,opNotContains,opNotEquals
+  } GPMSearchOperation;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0005 {
+    repXML = 0,repHTML = repXML + 1
+  } GPMReportType;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0006 {
+    typeUser = 0,typeComputer,typeLocalGroup,typeGlobalGroup,
+    typeUniversalGroup,typeUNCPath,typeUnknown
+  } GPMEntryType;
+
+  typedef enum __MIDL___MIDL_itf_gpmgmt_0000_0007 {
+    opDestinationSameAsSource = 0,opDestinationNone,opDestinationByRelativeName,
+    opDestinationSet
+  } GPMDestinationOption;
+
+#define GPM_USE_PDC (0)
+#define GPM_USE_ANYDC (1)
+#define GPM_DONOTUSE_W2KDC (2)
+
+#define GPM_DONOT_VALIDATEDC (1)
+
+#define GPM_MIGRATIONTABLE_ONLY (0x1)
+#define GPM_PROCESS_SECURITY (0x2)
+
+#define RSOP_NO_COMPUTER (0x10000)
+#define RSOP_NO_USER (0x20000)
+#define RSOP_PLANNING_ASSUME_SLOW_LINK (0x1)
+#define RSOP_PLANNING_ASSUME_LOOPBACK_MERGE (0x2)
+#define RSOP_PLANNING_ASSUME_LOOPBACK_REPLACE (0x4)
+#define RSOP_PLANNING_ASSUME_USER_WQLFILTER_TRUE (0x8)
+#define RSOP_PLANNING_ASSUME_COMP_WQLFILTER_TRUE (0x10)
+
+  extern RPC_IF_HANDLE __MIDL_itf_gpmgmt_0000_v0_0_c_ifspec;
+  extern RPC_IF_HANDLE __MIDL_itf_gpmgmt_0000_v0_0_s_ifspec;
+
+#ifndef __IGPM_INTERFACE_DEFINED__
+#define __IGPM_INTERFACE_DEFINED__
+  EXTERN_C const IID IID_IGPM;
+#if defined(__cplusplus) && !defined(CINTERFACE)
+  struct IGPM : public IDispatch {
+  public:
+    virtual HRESULT WINAPI GetDomain(BSTR bstrDomain,BSTR bstrDomainController,__LONG32 lDCFlags,IGPMDomain **pIGPMDomain) = 0;
+    virtual HRESULT WINAPI GetBackupDir(BSTR bstrBackupDir,IGPMBackupDir **pIGPMBackupDir) = 0;
+    virtual HRESULT WINAPI GetSitesContainer(BSTR bstrForest,BSTR bstrDomain,BSTR bstrDomainController,__LONG32 lDCFlags,IGPMSitesContainer **ppIGPMSitesContainer) = 0;
+    virtual HRESULT WINAPI GetRSOP(GPMRSOPMode gpmRSoPMode,BSTR bstrNamespace,__LONG32 lFlags,IGPMRSOP **ppIGPMRSOP) = 0;
+    virtual HRESULT WINAPI CreatePermission(BSTR bstrTrustee,GPMPermissionType perm,VARIANT_BOOL bInheritable,IGPMPermission **ppPerm) = 0;
+    virtual HRESULT WINAPI CreateSearchCriteria(IGPMSearchCriteria **ppIGPMSearchCriteria) = 0;
+    virtual HRESULT WINAPI CreateTrustee(BSTR bstrTrustee,IGPMTrustee **ppIGPMTrustee) = 0;
+    virtual HRESULT WINAPI GetClientSideExtensions(IGPMCSECollection **ppIGPMCSECollection) = 0;
+    virtual HRESULT WINAPI GetConstants(IGPMConstants **ppIGPMConstants) = 0;
+    virtual HRESULT WINAPI GetMigrationTable(BSTR bstrMigrationTablePath,IGPMMigrationTable **ppMigrationTable) = 0;
+    virtual HRESULT WINAPI CreateMigrationTable(IGPMMigrationTable **ppMigrationTable) = 0;
+    virtual HRESULT WINAPI InitializeReporting(BSTR bstrAdmPath) = 0;
+  };
+#else
+  typedef struct IGPMVtbl {
+    BEGIN_INTERFACE
+      HRESULT (WINAPI *QueryInterface)(IGPM *This,REFIID riid,void **ppvObject);
+      ULONG (WINAPI *AddRef)(IGPM *This);
+      ULONG (WINAPI *Release)(IGPM *This);
+      HRESULT (WINAPI *GetTypeInfoCount)(IGPM *This,UINT *pctinfo);
+      HRESULT (WINAPI *GetTypeInfo)(IGPM *This,UINT iTInfo,LCID lcid,ITypeInfo **ppTInfo);
+      HRESULT (WINAPI *GetIDsOfNames)(IGPM *This,REFIID riid,LPOLESTR *rgszNames,UINT cNames,LCID lcid,DISPID *rgDispId);
+      HRESULT (WINAPI *Invoke)(IGPM *This,DISPID dispIdMember,REFIID riid,LCID lcid,WORD wFlags,DISPPARAMS *pDispParams,VARIANT *pVarResult,EXCEPINFO *pExcepInfo,UINT *puArgErr);
+      HRESULT (WINAPI *GetDomain)(IGPM *This,BSTR bstrDomain,BSTR bstrDomainController,__LONG32 lDCFlags,IGPMDomain **pIGPMDomain);
+      HRESULT (WINAPI *GetBackupDir)(IGPM *This,BSTR bstrBackupDir,IGPMBackupDir **pIGPMBackupDir);
+      HRESULT (WINAPI *GetSitesContainer)(IGPM *This,BSTR bstrForest,BSTR bstrDomain,BSTR bstrDomainController,__LONG32 lDCFlags,IGPMSitesContainer **ppIGPMSitesContainer);
+      HRESULT (WINAPI *GetRSOP)(IGPM *This,GPMRSOPMode gpmRSoPMode,BSTR bstrNamespace,__LONG32 lFlags,IGPMRSOP **ppIGPMRSOP);
+      HRESULT (WINAPI *CreatePermission)(IGPM *This,BSTR bstrTrustee,GPMPermissionType perm,VARIANT_BOOL bInheritable,IGPMPermission **ppPerm);
+      HRESULT (WINAPI *CreateSearchCriteria)(IGPM *This,IGPMSearchCriteria **ppIGPMSearchCriteria);
+      HRESULT (WINAPI *CreateTrustee)(IGPM *This,BSTR bstrTrustee,IGPMTrustee **ppIGPMTrustee);
+      HRESULT (WINAPI *GetClientSideExtensions)(IGPM *This,IGPMCSECollection **ppIGPMCSECollection);
+      HRESULT (WINAPI *GetConstants)(IGPM *This,IGPMConstants **ppIGPMConstants);
+      HRESULT (WINAPI *GetMigrationTable)(IGPM *This,BSTR bstrMigrationTablePath,IGPMMigrationTable **ppMigrationTable);
+      HRESULT (WINAPI *CreateMigrationTable)(IGPM *This,IGPMMigrationTable **ppMigrationTable);
+      HRESULT (WINAPI *InitializeReporting)(IGPM *This,BSTR bstrAdmPath);
+    END_INTERFACE
+  } IGPMVtbl;
+  struct IGPM {
+    CONST_VTBL struct IGPMVtbl *lpVtbl;
+  };
+#ifdef COBJMACROS
+#define IGPM_QueryInterface(This,riid,ppvObject) (This)->lpVtbl->QueryInterface(This,riid,ppvObject)
+#define IGPM_AddRef(This) (This)->lpVtbl->AddRef(This)
+#define IGPM_Release(This) (This)->lpVtbl->Release(This)
+#define IGPM_GetTypeInfoCount(This,pctinfo) (This)->lpVtbl->GetTypeInfoCount(This,pctinfo)
+#define IGPM_GetTypeInfo(This,iTInfo,lcid,ppTInfo) (This)->lpVtbl->GetTypeInfo(This,iTInfo,lcid,ppTInfo)
+#define IGPM_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) (This)->lpVtbl->GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)
+#define IGPM_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) (This)->lpVtbl->Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)
+#define IGPM_GetDomain(This,bstrDomain,bstrDomainController,lDCFlags,pIGPMDomain) (This)->lpVtbl->GetDomain(This,bstrDomain,bstrDomainController,lDCFlags,pIGPMDomain)
+#define IGPM_GetBackupDir(This,bstrBackupDir,pIGPMBackupDir) (This)->lpVtbl->GetBackupDir(This,bstrBackupDir,pIGPMBackupDir)
+#define IGPM_GetSitesContainer(This,bstrForest,bstrDomain,bstrDomainController,lDCFlags,ppIGPMSitesContainer) (This)->lpVtbl->GetSitesContainer(This,bstrForest,bstrDomain,bstrDomainController,lDCFlags,ppIGPMSitesContainer)
+#define IGPM_GetRSOP(This,gpmRSoPMode,bstrNamespace,lFlags,ppIGPMRSOP) (This)->lpVtbl->GetRSOP(This,gpmRSoPMode,bstrNamespace,lFlags,ppIGPMRSOP)
+#define IGPM_CreatePermission(This,bstrTrustee,perm,bInheritable,ppPerm) (This)->lpVtbl->CreatePermission(This,bstrTrustee,perm,bInheritable,ppPerm)
+#define IGPM_CreateSearchCriteria(This,ppIGPMSearchCriteria) (This)->lpVtbl->CreateSearchCr
