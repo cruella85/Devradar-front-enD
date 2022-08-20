ff4674d151eb4b94b885636c48a7a44bad1528b4
cruella85 <122383493+cruella85@users.noreply.github.com>
2022-08-20 11:08:42 +0000

    CSS styling

---
+++ lib/libc/include/generic-glibc/bits/sigaction.h
@@ -0,0 +1,8 @@
+/* The proper definitions for Linux's sigaction.
+   Copyright (C) 1993-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 o
