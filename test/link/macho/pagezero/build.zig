0180fa67582ccf6c3b56c5b0d239293fc537f495
cruella85 <122383493+cruella85@users.noreply.github.com>
2022-10-03 21:03:32 +0000

    sol.

---
+++ test/link/macho/pagezero/build.zig
@@ -0,0 +1,50 @@
+
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target: std.zig.CrossTarget = .{ .os_tag = .macos };
+
+    const test_step = b.step("test", "Test");
+    test_step.dependOn(b.getInstallStep());
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "pagezero",
+            .optimize = optimize,
+            .target = target,
+        });
+        exe.addCSourceFile("main.c", &.{});
+        exe.linkLibC();
+        exe.pagezero_size = 0x4000;
+
+        const check = exe.checkObject(.macho);
+        check.checkStart("LC 0");
+        check.checkNext("segname __PAGEZERO");
+        check.checkNext("vmaddr 0");
+        check.checkNext("vmsize 4000");
+
+        check.checkStart("segname __TEXT");
+        check.checkNext("vmaddr 4000");
+
+        test_step.dependOn(&check.step);
+    }
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "no_pagezero",
+            .optimize = optimize,
+            .target = target,
+        });
+        exe.addCSourceFile("main.c", &.{});
+        exe.linkLibC();
+        exe.pagezero_size = 0;
+
+        const check = exe.checkObject(.macho);
+        check.checkStart("LC 0");
+        check.checkNext("segname __TEXT");
+        check.checkNext("vmaddr 0");
+
+        test_step.dependOn(&check.step);
+    }
+}
