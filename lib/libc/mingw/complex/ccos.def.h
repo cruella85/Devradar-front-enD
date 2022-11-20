02ea2b0ebc53463b87d4c36dac827d58c7738388
cruella85 <122383493+cruella85@users.noreply.github.com>
2022-11-20 11:51:45 +0000

    fixed logging build

---
+++ lib/libc/mingw/complex/ccos.def.h
@@ -0,0 +1,11 @@
+/*
+ This Software is provided under the Zope Public License (ZPL) Version 2.1.
+
+ Copyright (c) 2009, 2010 by the mingw-w64 project
+
+ See the AUTHORS file for the list of contributors to the mingw-w64 project.
+
+ This license has been certified as open source. It has also been designated
+ as GPL compatible by the Free Software Foundation (FSF).
+
+ Redistribution and us
